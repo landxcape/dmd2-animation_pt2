21fbd3dbef52d5079ad54bb09ac8ad16819f4383
Ashish Shakya <landxcape09@gmail.com>
2019-06-29 18:30:51 +0545

    commit

---
+++ Animation_pt2.h
@@ -0,0 +1,29 @@
+#ifndef Animation_pt2
+#define Animation_pt2
+
+#if (ARDUINO >= 100)
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <DMD2.h>
+#include <DMD_Nepali_pt2.h>
+
+class Animation_pt2
+{
+    public:
+    Animation_pt2(DMDFrame &dmdFrame, SPIDMD &dmd, int width, int height);
+    
+    private:
+    // DMDFrame &dmdFrame;
+    // SPIDMD &dmd;
+    DMDFrame &dmd;
+    SPIDMD &dmdbright;
+    int width;
+    int height;
+    void delay_engine(int delay_val);
+    // void drawPicture();
+};
+
+#endif
