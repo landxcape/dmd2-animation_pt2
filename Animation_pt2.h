ae97454631fbd0f5a4f9e314dd92dff9a2e7d85c
landxcape <landxcape09@gmail.com>
2019-12-14 17:48:22 +0545

    l

--- Animation_pt2.h
+++ Animation_pt2.h
@@ -30,6 +30,7 @@ public:
 
     // project specifics
     uint16_t starTailors(int, int, String type, String style = "none");
+    uint16_t hardford(int, int, String type, String style = "none");
     uint16_t diyoFoodland(int, int, String type, String style = "none");
     uint16_t unionLife(int, int, String type, String style = "none");
     uint16_t nec(int, int, String type, String style = "none");
