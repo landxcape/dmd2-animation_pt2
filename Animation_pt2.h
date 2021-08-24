dceb2570d0516e0c7de5f6402e3c44f5345d92a1
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2021-08-24 13:44:21 +0545

    update mausam meat mart

--- Animation_pt2.h
+++ Animation_pt2.h
@@ -36,12 +36,12 @@ public:
     // uint16_t unionLife(int, int, String type, String style = "none");
     // uint16_t jyotiLife(int, int, String type, String style = "none");
     // uint16_t nec(int, int, String type, String style = "none");
-    uint16_t nec_new(int, int, String type, String style = "none");
+    // uint16_t nec_new(int, int, String type, String style = "none");
     // uint16_t pkm(int, int, String type, String style = "none");
     // uint16_t rfr(int, int, String type, String style = "none");
     // uint16_t lmmcsl(int, int, String type, String style = "none");
     // uint16_t durga_bhawani(int, int, String type, String style = "none");
-    // uint16_t mausam_meat_mart(int, int, String type, String style = "none");
+    uint16_t mausam_meat_mart(int, int, String type, String style = "none");
 
 private:
     // DMDFrame &dmdFrame;
